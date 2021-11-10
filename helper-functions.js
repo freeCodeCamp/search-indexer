2f9a00657690dea06ed26f27ca425f95b96ae9c1
Kristofer Koishigawa <scissorsneedfoodtoo@gmail.com>
2021-11-10 16:40:16 +0900

    feat: add uk and ja indicies (#11)

--- helper-functions.js
+++ helper-functions.js
@@ -53,7 +53,9 @@ const indexNameMap = {
   'https://www.freecodecamp.org/espanol/news/': 'news-es',
   'https://chinese.freecodecamp.org/news/': 'news-zh',
   'https://www.freecodecamp.org/portuguese/news/': 'news-pt-br',
-  'https://www.freecodecamp.org/italian/news/': 'news-it'
+  'https://www.freecodecamp.org/italian/news/': 'news-it',
+  'https://www.freecodecamp.org/ukrainian/news/': 'news-uk',
+  'https://www.freecodecamp.org/japanese/news/': 'news-ja'
 };
 
 const setIndex = (url) => {
