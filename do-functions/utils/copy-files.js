7d2f632b4363a7d00c894754ac10e59be882781e
scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>
2024-10-28 16:50:28 +0900

    fix: improve handling for cases where a file we need to copy does not exist

--- do-functions/utils/copy-files.js
+++ do-functions/utils/copy-files.js
@@ -1,23 +1,34 @@
-// import { copyFileSync, readdirSync } from 'fs';
-import { copyFileSync } from 'fs';
+import { copyFileSync, existsSync } from 'fs';
 import { basename, join } from 'path';
 const __dirname = import.meta.dirname;
-
-// const hashnodeFunctionDirectoryNames = readdirSync(
-//   join(__dirname, '../packages/hashnode')
-// ).filter((directoryName) => !/(^|\/)\.[^\/\.]/g.test(directoryName));
 
 const platforms = ['hashnode'];
 const functionDirectoyNames = ['add-index'];
 const targetFilePaths = [
   join(__dirname, './helpers.js'),
   join(__dirname, '../package.json'),
-  join(__dirname, '../../.env')
+  join(__dirname, '../../.env') // TODO: remove this once we decide how to handle env vars on DO's platform
 ];
 
 platforms.forEach((platform) => {
   functionDirectoyNames.forEach((functionName) => {
     targetFilePaths.forEach((targetPath) => {
+      if (!existsSync(targetPath)) {
+        // TODO: Consider throwing an error here instead of logging a warning once we
+        // have a way to handle env vars on DO's platform
+        console.warn(`
+----------------------------------------------------
+Warning: File not found
+----------------------------------------------------
+The file following file was not found:
+${targetPath}
+
+Please make sure the file exists and try again.
+----------------------------------------------------
+`);
+        return;
+      }
+
       copyFileSync(
         targetPath,
         join(
