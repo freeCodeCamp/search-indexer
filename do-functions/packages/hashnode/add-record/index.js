77d3db802a18e85149f121aaa3eae8dd38efbfbd
scissorsneedfoodtoo <scissorsneedfoodtoo@gmail.com>
2024-10-29 16:05:56 +0900

    fix: rename add-index to add-record

--- do-functions/packages/hashnode/add-record/index.js
+++ do-functions/packages/hashnode/add-record/index.js
@@ -1,6 +1,6 @@
 import { algoliaClient, formatPost, getPost } from './helpers.js'; // Import from the same directory due to the pre deployment scripting process
 
-export const addIndex = async (req) => {
+export const addRecord = async (req) => {
   try {
     const { id } = req?.data?.post;
     const post = await getPost(id);
